8fa916fdd64cf4460a3de7b2d5b93e7271300baf
RePyITzy <127877297+RePyITzy@users.noreply.github.com>
2023-11-22 00:33:20 +0900

    Update main.py
    This is main.py for: "Эспрессо"

--- main.py
+++ main.py
@@ -5,6 +5,51 @@ import sqlite3
 import sys
 
 
+class AddCoffee(QMainWindow):
+  def __init__(self):
+    super(AddCoffee, self).__init__()
+    QInterface('add_coffee_form.ui', self)
+
+    self.bindUi()
+
+  def bindUi(self):
+    self.addButton.clicked.connect(self.addCoffee)
+
+  def addCoffee(self):
+    sort = self.sort.text()
+    roasting = self.roasting.text()
+    coffee = self.coffee.text()
+    taste = self.taste.text()
+    price = self.price.text()
+    capacity = self.capacity.text()
+
+    if not all([req for req in [sort, roasting, coffee, taste, price, capacity]]):
+      self.statusBar().showMessage('Не удалось добавить новое кофе.')
+      return
+
+    if not roasting.isdigit():
+      self.statusBar().showMessage('Не удалось добавить новое кофе.')
+      return
+
+    if not price.isdigit():
+      self.statusBar().showMessage('Не удалось добавить новое кофе.')
+      return
+
+    if not capacity.isdigit():
+      self.statusBar().showMessage('Не удалось добавить новое кофе.')
+      return
+
+    database = sqlite3.connect('coffee.sqlite')
+    cursor = database.cursor()
+
+    cursor.execute(
+      f"""INSERT INTO coffees(sort, roasting, coffee, taste, price, capacity)
+      VALUES('{sort}', {roasting}, '{coffee}', '{taste}', {price}, {capacity})"""
+    )
+
+    database.commit()
+
+
 class CoffeeViewer(QMainWindow):
   def __init__(self):
     super(CoffeeViewer, self).__init__()
